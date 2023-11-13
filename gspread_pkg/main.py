58be8f87e877dd9e4c07f328f96901a512e615b9
Timileyin <sadiquetimileyin@gmail.com>
2023-11-13 13:15:01 +0100

    file formatted

--- gspread_pkg/main.py
+++ gspread_pkg/main.py
@@ -17,8 +17,10 @@ link = "https://www.lusha.com/company-search/accounting/10/canada/193/page/2/"
 
 def chrome_driver():
     option = webdriver.ChromeOptions()
-    option.add_argument('--headless')
-    drivers = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=option)
+    option.add_argument("--headless")
+    drivers = webdriver.Chrome(
+        service=ChromeService(ChromeDriverManager().install()), options=option
+    )
     return drivers
 
 
@@ -71,15 +73,22 @@ def spreadsheet_format(filename, sheet_name, first_column, second_column):
     :param first_column: This is the name of the start column in the range in str format
     :param second_column: This is the name of the end column in the range in str format
     """
-    if (isinstance(filename, str) and isinstance(sheet_name, str)
-            and isinstance(first_column, str) and isinstance(second_column, str)):
-        formats = (select_spreadsheet(filename, sheet_name).format
-                   (f"{first_column}:{second_column}", {'textFormat': {'bold': True}}))
+    if (
+        isinstance(filename, str)
+        and isinstance(sheet_name, str)
+        and isinstance(first_column, str)
+        and isinstance(second_column, str)
+    ):
+        formats = select_spreadsheet(filename, sheet_name).format(
+            f"{first_column}:{second_column}", {"textFormat": {"bold": True}}
+        )
         logging.info("Worksheet columns format has been set")
         return formats
     else:
-        raise ValueError("The input expected a string object e.g 'A1' for the cols and rows input."
-                         "Wrong input expected a string object.")
+        raise ValueError(
+            "The input expected a string object e.g 'A1' for the cols and rows input."
+            "Wrong input expected a string object."
+        )
 
 
 def new_worksheet(filename, new_name, rows, columns):
@@ -91,8 +100,12 @@ def new_worksheet(filename, new_name, rows, columns):
     :param columns: Number of desired columns
     """
 
-    if (isinstance(rows, int) and isinstance(columns, int) and
-            isinstance(filename, str) and isinstance(new_name, str)):
+    if (
+        isinstance(rows, int)
+        and isinstance(columns, int)
+        and isinstance(filename, str)
+        and isinstance(new_name, str)
+    ):
         open_s = open_spreadsheet(filename)
         sheet_names = [s.title for s in open_s.worksheets()]
         if new_name not in sheet_names:
@@ -101,8 +114,10 @@ def new_worksheet(filename, new_name, rows, columns):
         else:
             raise FileExistsError("This worksheet already exists")
     else:
-        raise ValueError("The input expected a number/integer for the "
-                         "rows ands cols input, other inputs are strings")
+        raise ValueError(
+            "The input expected a number/integer for the "
+            "rows ands cols input, other inputs are strings"
+        )
 
 
 if __name__ == "__main__":
@@ -118,32 +133,39 @@ if __name__ == "__main__":
 
     num = 1
     linked = []
-    links = driver.find_elements(By.CSS_SELECTOR, value='.directory-content-box-inner a')
+    links = driver.find_elements(
+        By.CSS_SELECTOR, value=".directory-content-box-inner a"
+    )
     for link in links:
-        linked.append(link.get_attribute('href'))
+        linked.append(link.get_attribute("href"))
     for links in linked:
         num += 1
         driver.get(links)
         company_name = None
         while not company_name:
             try:
-                txt = driver.find_element(By.TAG_NAME, value='h1')
+                txt = driver.find_element(By.TAG_NAME, value="h1")
                 company_name = txt.text
             except NoSuchElementException:
                 company_name = "None"
         company_link = None
         while not company_link:
             try:
-                linke = driver.find_element(By.XPATH, value='/html/body/main/div[1]/div/section[1]/div/div[1]'
-                                                            '/div/div[2]/a')
-                company_link = linke.get_attribute('href')
+                linke = driver.find_element(
+                    By.XPATH,
+                    value="/html/body/main/div[1]/div/section[1]/div/div[1]"
+                    "/div/div[2]/a",
+                )
+                company_link = linke.get_attribute("href")
             except NoSuchElementException:
                 company_link = "None"
         linkedin = None
         while not linkedin:
             try:
-                texts = driver.find_element(By.CSS_SELECTOR, value='.company-details-socials a')
-                linkedin = texts.get_attribute('href')
+                texts = driver.find_element(
+                    By.CSS_SELECTOR, value=".company-details-socials a"
+                )
+                linkedin = texts.get_attribute("href")
             except NoSuchElementException:
                 linkedin = "None"
 
